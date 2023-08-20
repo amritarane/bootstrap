918c19d483a140077b25052204b091a0deb79399
amritarane <raneamrita123@gmail.com>
2023-08-20 11:55:56 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def MDB4(request):
+    return render(request,'MDB4.html')
+def MDB5(request):
+    return render(request,'MDB5.html')
